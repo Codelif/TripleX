517e66077b3c01759d88c131f68356eda8d18d64
Codelif <goharsh007@gmail.com>
2020-08-05 11:29:25 +0530

    Variables and Operations

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,5 +2,13 @@
 
 int main(){
     std::cout << "Hello, world";
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    std::cout << std::endl;
+    std::cout << a + b + c;  
+    std::cout << std::endl;
+    std::cout << a * b * c;
     return 0;
 }
