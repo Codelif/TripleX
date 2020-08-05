97a042033aba600dc53a4dfb31de21000c434fa7
Codelif <goharsh007@gmail.com>
2020-08-05 12:25:56 +0530

    Statements & Comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
+#include <iostream> //Preprocessor Directive
 
-int main()
+int main() //Main Function
 {
+    //Expression Statements
     std::cout << "You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
 
+    //Declaration Statements
     const int a = 4;
     const int b = 3;
     const int c = 2;
@@ -13,9 +15,10 @@ int main()
     const int sum = a + b + c;
     const int product = a * b * c;
 
+    //Expression Statements
     std::cout << std::endl;
     std::cout << sum << std::endl;
     std::cout << product << std::endl;
 
-    return 0;
+    return 0; // Return Statement
 }
