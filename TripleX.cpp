eac619fba4f16dea0e4e2493c31f97556627031d
Codelif <goharsh007@gmail.com>
2020-08-05 12:14:14 +0530

    const & Assigning Values

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello, world";
-    int a = 4;
-    int b = 3;
-    int c = 2;
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
+
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
 
     std::cout << std::endl;
-    std::cout << a + b + c;  
-    std::cout << std::endl;
-    std::cout << a * b * c;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
